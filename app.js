47e40eb778be97fad26ff9c239cd3543e7c244b6
Joshua Broqueza <joshuabroqueza@yahoo.com>
2022-08-11 14:39:25 +0800

    Adding item to cart

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
+const itemname = document.getElementById('item_name');
+const item_price = document.getElementById('item_price');
+
 open.addEventListener('click', function () {
-  console.log('hello');
+  document.getElementById(
+    'modaliD'
+  ).innerHTML += ` <div class="cartlist" id="cartlist_item">
+            <div class="added--itemname">${itemname.textContent}</div>
+            <div class="add--itemquantity">1</div>
+            <div class="add--itemprice">${item_price.textContent}</div>
+            </div> `;
 
   modal_container.classList.add('show');
 });
